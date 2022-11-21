9938c4d52f7fb04993dd5b81abe4a6b03b1a6a9a
Igor Augusto Dos Santos <igoraugusto1029@gmail.com>
2022-11-21 20:47:23 -0300

    Primeiro commit

---
+++ include/Secrets.example.h
@@ -0,0 +1,25 @@
+#include <pgmspace.h>
+
+#define SECRET
+
+#define THINGNAME "mochila"
+
+const char AWS_IOT_ENDPOINT[] = "a12s85dpg0xou0-ats.iot.sa-east-1.amazonaws.com";
+
+static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)KEY";
+
+static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+
+-----END RSA PRIVATE KEY-----
+)KEY";
+
+static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)EOF";
