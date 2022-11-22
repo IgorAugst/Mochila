c2548bb024c4df0b64f5785ca8eefaa35a48cff7
Igor Augusto Dos Santos <igoraugusto1029@gmail.com>
2022-11-21 21:06:13 -0300

    Atualiza exemplo

--- include/Secrets.example.h
+++ include/Secrets.example.h
@@ -6,13 +6,13 @@
 
 const char AWS_IOT_ENDPOINT[] = "a12s85dpg0xou0-ats.iot.sa-east-1.amazonaws.com";
 
-static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
+static const char CLIENT_CRT[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
 
 -----END CERTIFICATE-----
 )KEY";
 
-static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
+static const char CLIENT_PRIV[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
 
 -----END RSA PRIVATE KEY-----
