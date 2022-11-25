a0054424e57537de3612a69e2c16baa05d91f56e
Igor <igoraugusto1029@gmail.com>
2022-11-25 14:52:44 -0300

    Adiciona temperatura e umidade

--- web/index.js
+++ web/index.js
@@ -33,22 +33,35 @@ async function connect_websocket(){
 }
 
 var connection;
-var updateAcceptedTopic = "$aws/things/mochila/shadow/name/location/update/accepted";
+
+var updateMapAcceptedTopic = "$aws/things/mochila/shadow/name/location/update/accepted";
 var getMapAcceptedTopic = "$aws/things/mochila/shadow/name/location/get/accepted";
 var getMapTopic = "$aws/things/mochila/shadow/name/location/get";
+
+var updateTempAcceptedTopic = "$aws/things/mochila/shadow/name/weather/update/accepted";
+var getTempAcceptedTopic = "$aws/things/mochila/shadow/name/weather/get/accepted";
+var getTempTopic = "$aws/things/mochila/shadow/name/weather/get";
 
 async function main(){
 	console.log("Connecting to websocket");
 	connection = await connect_websocket();
-	connection.subscribe(updateAcceptedTopic, 0, (topic, payload) => {messageCallback(topic, payload)});
+	
+	connection.subscribe(updateMapAcceptedTopic, 0, (topic, payload) => {messageCallback(topic, payload)});
 	connection.subscribe(getMapAcceptedTopic, 0, (topic, payload) => {messageCallback(topic, payload)});
-	setTimeout(() => {connection.publish(getMapTopic, "", 0)}, 1000);
+	
+	connection.subscribe(updateTempAcceptedTopic, 0, (topic, payload) => {messageCallback(topic, payload)});
+	connection.subscribe(getTempAcceptedTopic, 0, (topic, payload) => {messageCallback(topic, payload)});
+	
+	setTimeout(() => {
+		connection.publish(getMapTopic, "", 0);
+		connection.publish(getTempTopic, "", 0);
+	}, 1000);
 	
 }
 
 function messageCallback(topic, payload){
 	console.log("Message received on topic: " + topic);
-	if (topic == updateAcceptedTopic || topic == getMapAcceptedTopic) {
+	if (topic == updateMapAcceptedTopic || topic == getMapAcceptedTopic) {
 		const decoder = new TextDecoder('utf8');
 		let message = decoder.decode(new Uint8Array(payload));
 		let json = JSON.parse(message);
@@ -60,10 +73,28 @@ function messageCallback(topic, payload){
 	if(topic == getMapAcceptedTopic){
 		connection.unsubscribe(getMapAcceptedTopic);
 	}
+
+	if (topic == updateTempAcceptedTopic || topic == getTempAcceptedTopic) {
+		const decoder = new TextDecoder('utf8');
+		let message = decoder.decode(new Uint8Array(payload));
+		let json = JSON.parse(message);
+		var temp = json["state"]["reported"]["temp"];
+		var hum = json["state"]["reported"]["hum"];
+		updateTemp(temp, hum);
+	}
+
+	if(topic == getTempAcceptedTopic){
+		connection.unsubscribe(getTempAcceptedTopic);
+	}
 }
 
 function updateMap(lat, lon){;
-	document.getElementById('iframe').src = "https://www.google.com/maps/embed/v1/place?q=" + lat + "," + lon + "&key=AIzaSyCdLrku76jCwReDCe26PjPvDTP3WFO7hIg";
+	//document.getElementById('iframe').src = "https://www.google.com/maps/embed/v1/place?q=" + lat + "," + lon + "&key=AIzaSyCdLrku76jCwReDCe26PjPvDTP3WFO7hIg";
 }
 
-//main();
+function updateTemp(temp, hum){
+	$("#temp").text(temp + "°C");
+	$("#hum").text(hum + "%");
+}
+
+main();
